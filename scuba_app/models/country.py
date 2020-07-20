d0b20b4355a8781a49d5c7dd67d31b3c1fe16c66
Deev <deevyaswain@gmail.com>
2020-07-20 22:54:43 +0400

    css and api added

--- scuba_app/models/country.py
+++ scuba_app/models/country.py
@@ -2,31 +2,30 @@ from .base import db
 from .base import ma
 #from marshmallow_sqlalchemy import ModelSchema
 
-class Country(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    #countries_dive_avg_depth = db.Column(db.String, nullable=False)
-    countries_name = db.Column(db.String, nullable=False)
-    #countries_dive_avg_depth = db.Column(db.String, nullable=False)
-    countries_dive_name = db.relationship('Sites', backref='country', lazy=True)
-    #latitude=db.Column(db.String, nullable=False)
-    #longitude=db.Column(db.String, nullable=False)
-# here i'm naming the fields of my database in this function that will call on the main class i.e. connected to my database
-    def __init__(self ,countries_name, countries_dive_name, countries_dive_avg_depth, latitude,longitude):
-        self.countries_dive_name=countries_dive_name
-        self.countries_name=countries_name
-        #self.countries_dive_avg_depth=countries_dive_avg_depth
-        #self.latitude=latitude
-        #self.longitude=longitude
-    def __repr__(self):
-        return '<Country %r>'%self.countries_dive_name
+
 
 class Sites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     site_name=db.Column(db.String, unique=True)
+    countries_name = db.Column(db.String, nullable=False)
     countries_dive_avg_depth = db.Column(db.String, nullable=False)
     latitude=db.Column(db.String, nullable=False)
     longitude=db.Column(db.String, nullable=False)
-    country_id=db.Column(db.Integer, db.ForeignKey('country.id'))
+
+    def __init__(self ,countries_name, countries_dive_name, countries_dive_avg_depth, latitude,longitude):
+        self.site_name=countries_dive_name
+        self.countries_name=countries_name
+        self.countries_dive_avg_depth=countries_dive_avg_depth
+        self.latitude=latitude
+        self.longitude=longitude
+
+    def __repr__(self):
+        return '<Country %r>'%self.countries_dive_name
+
+
+
+    
+
     
 
 '''
@@ -43,26 +42,18 @@ class CountrySchema(ma.Schema):
 '''
 
 
-class CountrySchema(ma.Schema):
-    class Meta:
-        fields = (
-            'id',
-            'countries_dive_name',
-            'countries_name',
-            
-            
-        )
+
 class SitesSchema(ma.Schema):
     class Meta:
         fields = (
             'id',
-            'countries_dive_name',
+            'site_name',
             'countries_dive_avg_depth',
             'latitude',
-            'longitude'
+            'longitude',
+            'countries_name'
         )
 
-country_schema = CountrySchema()
-countries_schema = CountrySchema(many=True)
+
 sites_schema=SitesSchema(many=True)
 site_schema=SitesSchema()
--- scuba_app/routes/user/__init__.py
+++ scuba_app/routes/user/__init__.py
@@ -1,3 +1,4 @@
+import arrow
 import requests
 from flask import   session, Blueprint, request, render_template, redirect, flash, url_for, current_app, jsonify, render_template_string
 from .forms import CountryForm
@@ -6,9 +7,7 @@ from .helperfunc import conversion
 #from .forms import RegisterForm, SignupForm
 from ...models.base import db
 from ...models.user import User
-from ...models.country import (Country,
-                               country_schema,
-                               countries_schema, SitesSchema,Sites, site_schema)
+from ...models.country import (sites_schema,Sites, site_schema)
 from ...secrets import MAPBOX
 
 
@@ -31,12 +30,8 @@ def coordinata():
         search_value=form['countries_name']
         print(search_value)
         search="%{}%".format(search_value)
-        trial=Country.query.filter_by(countries_name="Alabama")
-        print(trial.id)
         
-        
-        
-        coordinate=Sites.query.filter(Country.countries_name.like(search)).all()
+        coordinate=Sites.query.filter(Sites.countries_name.like(search)).all()
         #coordinate=Sites.query.filter(Sites.site_name.like(search)).all()
         #coordinate= Sites.query.filter_by(countries_name="Alabama").first()
         
@@ -51,16 +46,17 @@ def coordinata():
         #return coordinate_var
         #print(coordinate)
         print('it reaches here 1')
-        print(listo)
-        print(coordinate)
+        
         
 
-        coordinate_output= countries_schema.dump(coordinate)
+        coordinate_output= sites_schema.dump(coordinate)
 
-        #session['my_var'] = array
+        session['my_var'] = array
         session['coord_var']= coordinate_output
         print('it reaches here x')
-        print(coordinate_output)
+        print(array)
+        
+        
 
         #return redirect(url_for('userRoute.test'))
 
@@ -71,13 +67,13 @@ def coordinata():
 
 @userRoute.route('/post_user', methods=['GET','POST'])
 def post_user():
-    myCountry=Country.query.limit(30).all()
+    myCountry=Sites.query.limit(30).all()
 
     if request.method=="POST":
         form=request.form
         search_value=form['countries_name']
         search="%{}%".format(search_value)
-        arr=Country.query.filter(Country.countries_name.like(search)).all()
+        arr=Sites.query.filter(Sites.countries_name.like(search)).all()
 
 
 
@@ -104,33 +100,98 @@ def simple():
 
 @userRoute.route('/meh/<country>', methods=['GET'])
 def index(country):
-
-
-
-    cities =Country.query.filter_by(countries_name="{}".format(country)).first()
-
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=230b7544b48513a794a7284e48f2ca63'
+    cities=Sites.query.filter_by(site_name="{}".format(country)).first()
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid=230b7544b48513a794a7284e48f2ca63'
 
     weather_data = []
     lat=conversion(cities.latitude)
     lon=conversion(cities.longitude)
+    print(lat, lon, 'string : latlong')
     
-    r = requests.get(url.format(cities.countries_name)).json()
+    #r = requests.get(url.format(cities.countries_name)).json()
+    r1 =requests.get(url.format(lat,lon)).json()
+    
 
-    '''weather = { 
-        'city' : cities.countries_name,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
+    weather = { 
+        'city' : cities.site_name,
+        'temperature' : r1['main']['temp'],
+        'description' : r1['weather'][0]['description'],
+        'wind_speed': r1['wind']['speed'],
         
 
-    }'''
-    weather=[{'city' : cities.countries_name},{'temperature' : r['main']['temp']},{'description' : r['weather'][0]['description']}]
+    }
+
+
+
+    start = arrow.now().floor('day')
+    end = arrow.now().shift(days=1).floor('day')
+
+    response1 = requests.get(
+    'https://api.stormglass.io/v2/tide/extremes/point',
+    params={
+        'lat': lat,
+        'lng': lon,
+        'start': start.to('UTC').timestamp,  # Convert to UTC timestamp
+        'end': end.to('UTC').timestamp,  # Convert to UTC timestam
+    },
+    headers={
+        'Authorization': '4604db58-c6ab-11ea-870a-0242ac130002-4604dc5c-c6ab-11ea-870a-0242ac130002'
+    }
+    )
+    tide_data = response1.json()
+    tidal_arr=[]
+    for i in range(2):
+        tidal={
+            'height': tide_data['data'][i]['height'],
+            'time': tide_data['data'][i]['time'],
+            'type': tide_data['data'][i]['type'],
+        }
+        tidal_arr.append(tidal)
+    print(tide_data)
+
+
+
+
+
+
+
+
+    
+    response2 = requests.get(
+        'https://api.stormglass.io/v2/weather/point',
+        params={
+            'lat': lat,
+            'lng': lon,
+            'params': ['waterTemperature'],
+             # Convert to UTC timestamp
+        },
+        headers={
+            'Authorization': '4604db58-c6ab-11ea-870a-0242ac130002-4604dc5c-c6ab-11ea-870a-0242ac130002'
+        }
+        )
+    wave_data=response2.json()
+    
+    waves={'water_temperature': wave_data['hours'][0]['waterTemperature']['noaa']}
+    #waves_arr.append(waves)
+    print(waves)
+
+
+    
+    
+    
+
+    
+    
+    #weather=[{'city' : cities.site_name},{'temperature' : r1['main']['temp']},{'description' : r1['weather'][0]['description']},{'wind_speed':r1['wind']['speed']}]
     weather_data.append(weather)
-    print(weather_data)
+    print(weather)
     
     session['my_api']=weather
     session['my_coord']=[lon, lat]
-    print(lat, lon, 'string : latlong')
+    session['my_site_name']=(cities.site_name, cities.countries_dive_avg_depth)
+    session['tide']=tidal_arr
+    session['waves']=waves
+   
     return redirect(url_for('userRoute.maps'))
 
 
@@ -157,9 +218,14 @@ def maps():
     #searcher1=[{1:1},{2:2}]
     
     output = session.get('coord_var', [{1:1},{2:2}])
-    #my_var = session.get('my_var', None)
+    my_var = session.get('my_var', None)
     my_weather=session.get('my_api',[{1:1},{2:2}])
     my_coord=session.get('my_coord', None)
+    sito=session.get('my_site_name',None)
+    tide=session.get('tide',[{1:1},{2:2}])
+    waves=session.get('waves',[{1:1},{2:2}])
+    
+    
     searcher1=(output)
     
 
@@ -172,8 +238,8 @@ def maps():
         
 
 
-    return render_template('mapbox.html',
-        mapbox_access_token=MAPBOX,tide='1.24',climate='27', coord=my_coord, searcher=searcher1, weather_data=my_weather)
+    return render_template('mapbox.html',mapbox_access_token=MAPBOX,tide='1.24',climate='27', coord=my_coord,
+     searcher=searcher1, weather_data=my_weather,sito=sito,coordinator=my_var,tidal=tide,waves=waves)
 
     
     
